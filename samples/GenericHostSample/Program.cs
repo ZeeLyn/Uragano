2f8463b7fde833c7c9c866a1696062e140c370ce
wangzelin <315872858@qq.com>
2019-02-21 15:25:26 +0800

    Update Program.cs

--- samples/GenericHostSample/Program.cs
+++ samples/GenericHostSample/Program.cs
@@ -5,7 +5,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Uragano.Configuration.Consul;
 using Uragano.Consul;
 using Uragano.Core;
 using Uragano.Logging.Exceptionless;
